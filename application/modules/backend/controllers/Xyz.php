5901e21e369995e3da280d167f2c36372d207fa2
warman suganda <warman.suganda@gmail.com>
2017-11-22 21:15:32 +0700

    setup HMVC done

---
+++ application/modules/backend/controllers/Xyz.php
@@ -0,0 +1,13 @@
+<?php
+class Xyz extends MX_Controller 
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+    	$this->slice->view("backend/xyz/index");
+    }
+}
