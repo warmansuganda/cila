7870f573dc6fd110a72b8f1a3e9b29ed5cde2790
warman suganda <warman.suganda@gmail.com>
2017-11-24 18:04:09 +0700

    setup migration and seeder

---
+++ application/controllers/Migrate.php
@@ -0,0 +1,13 @@
+<?php
+
+class Migrate extends CI_Controller {
+
+    public function index() {
+        $this->load->library('migration');
+
+        if ($this->migration->current() === FALSE) {
+            show_error($this->migration->error_string());
+        }
+    }
+
+}
